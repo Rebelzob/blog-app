4b96f55e20f13dbaeac314168f35055f876336c2
Rebelzob <karimdavid07@gmail.com>
2023-10-25 19:48:26 +0100

    implemented after_save on update_post_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,15 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  def update_post_counter
-    user.update(posts_counter: user.posts.count)
-  end
+  after_save :update_post_counter
 
   def five_recent_comments_from_post
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+  
+  def update_post_counter
+    user.update(posts_counter: user.posts.count)
   end
 end
