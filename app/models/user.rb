b7074dd07a2245d61426c1745dbaf0f17661915a
Rebelzob <karimdavid07@gmail.com>
2023-10-26 22:03:17 +0100

    Added validations requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  validates :name, presence: true, uniqueness: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+
   def three_last_posts
     posts.order(created_at: :desc).limit(3)
   end
