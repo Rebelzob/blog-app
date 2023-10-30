d0b9cd7f247fa8a786292b4d05681328598459c7
Rebelzob <karimdavid07@gmail.com>
2023-10-30 19:57:04 +0000

    created methods for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.where(author_id: params["user_id"]).all
+  end
+
+  def show
+    @user = User.find_by(id: params["user_id"])
+    @post = @user.posts.find_by(id: params["id"])
+  end
+end
