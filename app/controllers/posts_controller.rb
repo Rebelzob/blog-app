9f3c498e1c849bdf5bbc4c21b78f25e6d13477e3
Rebelzob <karimdavid07@gmail.com>
2023-11-02 21:30:49 +0000

    modified post controller and added create and new methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,28 @@ class PostsController < ApplicationController
   def show
     @posts = @user.posts.find(params[:id])
   end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.build(post_params)
+
+    if @post.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params
+      .require(:post)
+      .permit(:title, :text)
+  end
 end
