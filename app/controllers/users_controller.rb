bc132fe4738b61f46ae8c5cbb1a77973bb9a8007
Benson Kiai <benson.kiai86@gmail.com>
2023-11-10 00:04:05 +0300

    Add authorization to UsersController#index This commit adds authorization to UsersController#index by checking if the current user is authorized to view the index page. If the user is not authorized, they are redirected to their own user page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
+    authorize! :index, User
+
+    redirect_to user_path(current_user) if current_user && !current_user.admin?
     @users = User.all
   end
 
