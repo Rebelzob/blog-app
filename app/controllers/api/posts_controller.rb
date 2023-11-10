1e9f1ae845dc2b13c51674723478a8b47ea6ddbf
Benson Kiai <benson.kiai86@gmail.com>
2023-11-11 01:50:36 +0300

    Add index action to retrieve user's posts in API

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,4 +2,9 @@ class Api::PostsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:index]
   before_action :set_user, only: [:index]
 
+  def index
+    @posts = @user.posts
+    render json: @posts
+  end
+
 end
